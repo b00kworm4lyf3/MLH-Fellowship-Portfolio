a5be486c8ca5132d27fb8843319400fef4e4f6ec
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-06-19 13:58:21 -0600

    stubbed out hobby page with placeholder data

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
+from .data import hobbies
+
 load_dotenv()
 app = Flask(__name__)
 
@@ -9,10 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
-@app.route('/hobbies')
-def hobbies():
-    return render_template('hobbies.html', title = "My Hobbies", url = os.getenv("URL"))
 
+@app.route('/hobbies')
+def hobbyPage():
+    return render_template('hobbies.html', title = "My Hobbies", url = os.getenv("URL"), hobbies = hobbies)
 
 @app.route('/about')
 def about():
