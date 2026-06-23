fe0b57ff5b46f3a262946fdf761fe1b39a45c0b0
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-06-22 20:42:44 -0600

    hardcoded filler removed, work page no longer has certifications, recognitions, or badges though as I felt it wasn't a necessity. Could be added again later after a bit more thought. Also fixed up anchor tags and img srcs to use url_for()

--- app/__init__.py
+++ app/__init__.py
@@ -2,30 +2,30 @@ import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
-from .data import hobbies
+from .data import user, hobbies, exp
 
 load_dotenv()
 app = Flask(__name__)
 
-LINKS = [{"name": "About",   "endpoint": "about"},
-         {"name": "Work",    "endpoint": "work"},
-         {"name": "Hobbies", "endpoint": "hobbyPage"}]
+LINKS = [{"name": "About",   "endpoint": "about", "tagline": "About Me"},
+         {"name": "Work",    "endpoint": "work", "tagline": "Work Experience"},
+         {"name": "Hobbies", "endpoint": "hobby", "tagline": "My Hobbies"}]
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow")
+    return render_template('index.html', title="MLH Fellow", user = user)
 
 @app.route('/hobbies')
-def hobbyPage():
+def hobby():
     return render_template('hobbies.html', title = "My Hobbies", hobbies = hobbies)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title="About Me")
+    return render_template('about.html', title="About Me", user = user)
 
 @app.route('/work')
 def work():
-    return render_template('work.html', title="Work Experience")
+    return render_template('work.html', title="Work Experience", exp = exp)
 
 @app.context_processor
 def nav():
